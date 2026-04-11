48f87dbc366bead9d7e5282e06ad72d4f8cc0e84
varshinipatel <varshinipatel17@gmail.com>
2026-04-11 12:43:59 +0530

    Switch to SMTP port 587 for better cloud compatibility

--- utils/otpUtils.js
+++ utils/otpUtils.js
@@ -38,8 +38,8 @@ function getTransporter() {
   console.log("[Email] Initializing global transporter instance...");
   transporterInstance = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false, // Use STARTTLS
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
